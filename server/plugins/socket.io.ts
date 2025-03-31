ebb5e7af7a1544f54a12459eb4e3fcc8bd181c84
rita <wadeew2016@yandex.ru>
2025-03-31 14:38:19 +0500

    socket2

---
+++ server/plugins/socket.io.ts
@@ -0,0 +1,59 @@
+import type { NitroApp } from "nitropack";
+
+import { Server as Engine } from "engine.io";
+
+import { Server } from "socket.io";
+
+import { defineEventHandler } from "h3";
+
+
+export default defineNitroPlugin((nitroApp: NitroApp) => {
+
+  const engine = new Engine();
+
+  const io = new Server();
+
+
+  io.bind(engine);
+
+  let counter = 0;
+
+  io.on("connection", (socket) => {
+
+    counter++
+
+    io.emit("hello", counter);
+
+
+  });
+
+
+  nitroApp.router.use("/socket.io/", defineEventHandler({
+
+    handler(event) {
+
+      engine.handleRequest(event.node.req, event.node.res);
+
+      event._handled = true;
+
+    },
+
+    websocket: {
+
+      open(peer) {
+
+        // @ts-expect-error private method and property
+
+        engine.prepare(peer._internal.nodeReq);
+
+        // @ts-expect-error private method and property
+
+        engine.onWebSocket(peer._internal.nodeReq, peer._internal.nodeReq.socket, peer.websocket);
+
+      }
+
+    }
+
+  }));
+
+});
